9e674b5e2c8535d3d18f0e02e6fbf085540d1be1
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-17 17:30:37 -0600

    Fix command name from 'Resumir' to 'Resume'

--- commands/prefixed/utilresume.js
+++ commands/prefixed/utilresume.js
@@ -37,10 +37,10 @@ const data = {
       return ctx.send({
         embeds: [
           new EmbedBuilder()
-            .setAuthor({ name: "Comando Resumir", iconURL: ctx.bot.user.displayAvatarURL() })
+            .setAuthor({ name: "Comando Resume", iconURL: ctx.bot.user.displayAvatarURL() })
             .setDescription(
               `**Usos:**\nResume un texto de 100 caracteres o más` +
-              `\n\n**Aliases:**\n\`resume\`, \`summarize\`` +
+              `\n\n**Aliases:**\n\`resumir\`, \`summarize\`` +
               `\n\n\`\`\`js\n.resumir <texto>\nEjemplo: .resumir en terminos de reproducción entre hombres humanos y Pokémon hembras, Vaporeon es el...\`\`\``
             )
             .setColor(RED),
