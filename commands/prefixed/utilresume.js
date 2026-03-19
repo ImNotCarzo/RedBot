3a81090002a96aee7b71abc484954e1340d6d967
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-19 12:22:13 -0600

    Fix embed author and color in utilresume command

--- commands/prefixed/utilresume.js
+++ commands/prefixed/utilresume.js
@@ -37,11 +37,13 @@ const data = {
       return ctx.send({
         embeds: [
           new EmbedBuilder()
+            .setAuthor({ name: "Comando Resume", iconURL: ctx.bot.user.displayAvatarURL() })
             .setDescription(
-              `**Uso:**\n\`.resume <texto>\`\n\n` +
-              `Mínimo 100 caracteres.`
+              `**Usos:**\nResume un texto de 100 caracteres o más` +
+              `\n\n**Aliases:**\n\`resumir\`, \`summarize\`` +
+              `\n\n\`\`\`js\n.resume <texto>\nEjemplo: .resume en terminos de reproducción entre hombres humanos y Pokémon hembras, Vaporeon es el...\`\`\``
             )
-            .setColor(COLOR),
+            .setColor(RED),
         ],
       });
     }
