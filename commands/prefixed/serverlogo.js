ec0e9fcdd9607fb69dc84c4e1d0974285511b9c1
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 23:06:19 -0600

    Remove server logo command implementation
    Removed the implementation of the server logo command.

--- commands/prefixed/serverlogo.js
+++ commands/prefixed/serverlogo.js
@@ -1,4 +1,3 @@
-// commands/prefixed/serverlogo.js
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder } = require("discord.js");
 
@@ -11,26 +10,7 @@ const data = {
     as_slash: false,
   }),
 
-  async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-      if (!guild.iconURL()) return ctx.send("Este servidor no tiene logo");
-
-      const url = guild.iconURL({ size: 4096, extension: "png" });
-      const embed = new EmbedBuilder()
-        .setTitle(`Logo de ${guild.name}`)
-        .setURL(url)
-        .setImage(url)
-        .setColor("#ff383d")
-        .setTimestamp();
-
-      await ctx.send({ embeds: [embed] });
-    } catch (err) {
-      console.error("Error en logo:", err);
-      await ctx.send("No se pudo obtener el logo");
-    }
-  },
+  async code(ctx) {},
 };
 
 module.exports = { data };
