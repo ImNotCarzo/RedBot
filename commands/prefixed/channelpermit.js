cf6dc047346ce7cc4543db284eb95e8895509d72
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 22:15:38 -0600

    Refactor channel permit command to remove checks

--- commands/prefixed/channelpermit.js
+++ commands/prefixed/channelpermit.js
@@ -1,7 +1,7 @@
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const sendLog = require("../../utils/sendLog");
-const { GREEN, RED } = require("../../utils/colors");
+const { RED } = require("../../utils/colors");
 
 const data = {
   data: new CommandBuilder({
@@ -13,14 +13,6 @@ const data = {
   }),
 
   async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-
-      const member  = ctx.message?.mentions?.members?.first();
-      const channel = ctx.message?.mentions?.channels?.first() ?? ctx.channel;
-
-      if (!member) {
         const bot = ctx.bot.user;
         const paramerror = new EmbedBuilder()
           .setAuthor({ name: "Comando Permit", iconURL: bot.displayAvatarURL() })
@@ -32,43 +24,7 @@ const data = {
           .setColor(RED);
 
         return ctx.send({ embeds: [paramerror] });
-      }
-
-      const modTag = ctx.author?.tag ?? ctx.author?.username;
-
-      if (!ctx.member.permissions.has(PermissionFlagsBits.ManageChannels))
-        return ctx.send("No tienes el permiso `ManageChannels`");
-
-      if (!guild.members.me.permissions.has(PermissionFlagsBits.ManageChannels))
-        return ctx.send("No tengo permiso para editar canales");
-
-      await channel.permissionOverwrites.edit(member, {
-        ViewChannel:  true,
-        SendMessages: true,
-      }, { reason: `${modTag}: channel permit` });
-
-      const publicEmbed = new EmbedBuilder()
-        .setDescription(`${member} ahora tiene acceso a ${channel}`)
-        .setColor(GREEN)
-        .setTimestamp();
-
-      await ctx.send({ embeds: [publicEmbed] });
-
-      const logEmbed = new EmbedBuilder()
-        .setTitle("Acceso concedido")
-        .setColor(GREEN)
-        .addFields(
-          { name: "Usuario",   value: `${member.user.tag} (\`${member.id}\`)`, inline: true },
-          { name: "Canal",     value: `${channel} (\`${channel.id}\`)`,        inline: true },
-          { name: "Moderador", value: modTag,                                   inline: true },
-        )
-        .setTimestamp();
-
-      await sendLog(guild, logEmbed);
-    } catch {
-      await ctx.send("No se pudo dar acceso al usuario");
-    }
-  },
+      },
 };
 
 module.exports = { data };
