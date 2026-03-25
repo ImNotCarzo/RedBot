351fa7d8a4fc52d7f1d10099fe3b517a1175f031
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 23:03:20 -0600

    Remove server banner command implementation
    Removed the implementation of the server banner command.

--- commands/prefixed/serverbanner.js
+++ commands/prefixed/serverbanner.js
@@ -1,6 +1,5 @@
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder } = require("discord.js");
-
 const COLOR = "#ff383d";
 
 const data = {
@@ -12,26 +11,7 @@ const data = {
     as_slash: false,
   }),
 
-  async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-      if (!guild.banner) return ctx.send("Este servidor no tiene banner");
-
-      const url = guild.bannerURL({ size: 4096 });
-      const embed = new EmbedBuilder()
-        .setTitle(`Banner de ${guild.name}`)
-        .setURL(url)
-        .setImage(url)
-        .setColor(COLOR)
-        .setTimestamp();
-
-      await ctx.send({ embeds: [embed] });
-    } catch (err) {
-      console.error("Error en serverbanner:", err);
-      await ctx.send("No se pudo obtener el banner del servidor");
-    }
-  },
+  async code(ctx) {},
 };
 
 module.exports = { data };
