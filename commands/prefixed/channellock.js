a8989f09a9fee2ae6fae6d5aba90b08a71e58542
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 22:13:57 -0600

    Update channellock.js

--- commands/prefixed/channellock.js
+++ commands/prefixed/channellock.js
@@ -12,45 +12,7 @@ const data = {
     as_slash: false,
   }),
 
-  async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-
-      const channel = ctx.message?.mentions?.channels?.first() ?? ctx.channel;
-      const modTag  = ctx.author?.tag ?? ctx.author?.username;
-
-      if (!ctx.member.permissions.has(PermissionFlagsBits.ManageChannels))
-        return ctx.send("No tienes el permiso `ManageChannels`");
-
-      if (!guild.members.me.permissions.has(PermissionFlagsBits.ManageChannels))
-        return ctx.send("No tengo permiso para editar canales");
-
-      await channel.permissionOverwrites.edit(guild.roles.everyone, {
-        SendMessages: false,
-      }, { reason: `${modTag}: channel lock` });
-
-      const publicEmbed = new EmbedBuilder()
-        .setDescription(`**${channel} fue cerrado**`)
-        .setColor(RED)
-        .setTimestamp();
-
-      await ctx.send({ embeds: [publicEmbed] });
-
-      const logEmbed = new EmbedBuilder()
-        .setTitle("Canal bloqueado")
-        .setColor(RED)
-        .addFields(
-          { name: "Canal",     value: `${channel} (\`${channel.id}\`)`, inline: true },
-          { name: "Moderador", value: modTag,                            inline: true },
-        )
-        .setTimestamp();
-
-      await sendLog(guild, logEmbed);
-    } catch {
-      await ctx.send("No se pudo bloquear el canal");
-    }
-  },
+  async code(ctx) {},
 };
 
 module.exports = { data };
