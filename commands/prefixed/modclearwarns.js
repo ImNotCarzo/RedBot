51b116834a8aac08e2f843c9eaef73922f746d94
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 22:29:11 -0600

    Refactor clearwarns command for simplicity
    Removed unnecessary checks and streamlined the clear warnings command.

--- commands/prefixed/modclearwarns.js
+++ commands/prefixed/modclearwarns.js
@@ -1,9 +1,6 @@
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
-const Warn = require("../../models/Warn");
-const sendLog = require("../../utils/sendLog");
-const { GREEN, RED } = require("../../utils/colors");
-const { resolveMember } = require("../../utils/helpers");
+const { RED } = require("../../utils/colors");
 
 const data = {
   data: new CommandBuilder({
@@ -15,13 +12,6 @@ const data = {
   }),
 
   async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-
-      const input = ctx.args?.[0] || null;
-      const member = await resolveMember(ctx, input);
-      if (!member) {
         const bot = ctx.bot.user;
         const paramerror = new EmbedBuilder()
           .setAuthor({ name: "Comando Clearwarns", iconURL: bot.displayAvatarURL() })
@@ -33,31 +23,7 @@ const data = {
           .setColor(RED);
 
         return ctx.send({ embeds: [paramerror] });
-      }
-
-      if (!ctx.member.permissions.has(PermissionFlagsBits.ModerateMembers))
-        return ctx.send("No tienes el permiso `ModerateMembers`");
-
-      const result = await Warn.deleteMany({ guildId: guild.id, userId: member.id });
-
-      if (!result.deletedCount)
-        return ctx.send(`${member.user.tag} no tiene advertencias`);
-
-      const count = result.deletedCount;
-      const plural = count > 1 ? "advertencias" : "advertencia";
-      const username = member.user.globalName || member.user.username;
-
-      const embed = new EmbedBuilder()
-        .setDescription(`Se eliminaron **${count}** ${plural} de **${username}**`)
-        .setColor(GREEN)
-        .setTimestamp();
-
-      await ctx.send({ embeds: [embed] });
-      await sendLog(guild, embed);
-    } catch {
-      await ctx.send("No se pudieron limpiar las advertencias");
-    }
-  },
+      },
 };
 
 module.exports = { data };
