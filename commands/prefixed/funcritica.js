ff9d79d27ac90ffa089bd9e92c5e69261fe88e87
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 23:25:45 -0600

    Refactor funcritica command for improved structure

--- commands/prefixed/funcritica.js
+++ commands/prefixed/funcritica.js
@@ -1,6 +1,5 @@
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder } = require("discord.js");
-const { getAI } = require("../../utils/ai");
 const { RED } = require("../../colors");
 
 const data = {
@@ -13,19 +12,22 @@ const data = {
   }),
 
   async code(ctx) {
-      return ctx.send({
-        embeds: [
-          new EmbedBuilder()
-            .setAuthor({ name: "Comando Critica", iconURL: ctx.bot.user.displayAvatarURL() })
-            .setDescription(
-              `**Usos:**\nGenera una crítica despiadada de algo` +
+    return ctx.send({
+      embeds: [
+        new EmbedBuilder()
+          .setAuthor({
+            name: "Comando Critica",
+            iconURL: ctx.bot.user.displayAvatarURL(),
+          })
+          .setDescription(
+            `**Usos:**\nGenera una crítica despiadada de algo` +
               `\n\n**Aliases:**\n\`criticar\`, \`criticize\`` +
               `\n\n\`\`\`js\n.critica <tema>\nEjemplo: .critica la chochoinflación\`\`\``
-            )
-            .setColor(RED),
-          ],
-      }),
-        },
+          )
+          .setColor(RED),
+      ],
+    });
+  },
 };
 
 module.exports = { data };
