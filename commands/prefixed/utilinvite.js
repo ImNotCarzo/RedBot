a714730ec4c70db0eef412623d2e5de918bd7709
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 23:16:25 -0600

    Remove invite button code from utilinvite.js
    Removed the invite button functionality from utilinvite.js.

--- commands/prefixed/utilinvite.js
+++ commands/prefixed/utilinvite.js
@@ -9,30 +9,8 @@ const data = {
     as_prefix: true,
     as_slash: false,
   }),
-  params: new ParamsBuilder(),
-
-  async code(ctx) {
-    try {
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setLabel("Invitar")
-          .setStyle(ButtonStyle.Link)
-          .setURL("https://discord.com/oauth2/authorize?client_id=1020772849906098186"),
-        new ButtonBuilder()
-          .setLabel("Soporte")
-          .setStyle(ButtonStyle.Link)
-          .setURL("https://discord.gg/b8AKKaNWU6"),
-      );
-
-      await ctx.send({
-        content: "https://discord.com/oauth2/authorize?client_id=1020772849906098186",
-        components: [row],
-      });
-    } catch (err) {
-      console.error("[invite]", err);
-      await ctx.send("Algo salió mal");
-    }
-  },
+  
+  async code(ctx) {},
 };
 
 module.exports = { data };
