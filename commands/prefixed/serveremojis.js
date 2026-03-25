9a37f63b95a074a67e4a10e0fc96f589a18ff6bc
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 23:03:50 -0600

    Refactor server emojis command to empty function
    Removed the emoji retrieval logic from the command.

--- commands/prefixed/serveremojis.js
+++ commands/prefixed/serveremojis.js
@@ -10,26 +10,7 @@ const data = {
     as_slash: false,
   }),
 
-  async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-
-      const emojis = guild.emojis.cache.map((e) => e.toString());
-      if (!emojis.length) return ctx.send("Este servidor no tiene emojis");
-
-      const embed = new EmbedBuilder()
-        .setTitle(`Emojis de ${guild.name} (${emojis.length})`)
-        .setDescription(emojis.join(" "))
-        .setColor("#ff383d")
-        .setTimestamp();
-
-      await ctx.send({ embeds: [embed] });
-    } catch (err) {
-      console.error("Error en emojis:", err);
-      await ctx.send("No se pudo obtener los emojis");
-    }
-  },
+  async code(ctx) {},
 };
 
 module.exports = { data };
