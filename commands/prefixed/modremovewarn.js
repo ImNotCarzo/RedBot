ac4598b5a0a78acf0caa1a85d8d80337d8aa9964
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-03-24 22:34:57 -0600

    Refactor removewarn command to simplify logic
    Removed unnecessary checks and logging for the removewarn command.

--- commands/prefixed/modremovewarn.js
+++ commands/prefixed/modremovewarn.js
@@ -1,8 +1,6 @@
 const { CommandBuilder } = require("erine");
 const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
-const Warn = require("../../models/Warn");
-const sendLog = require("../../utils/sendLog");
-const { GREEN, RED } = require("../../utils/colors");
+const { RED } = require("../../utils/colors");
 
 const data = {
   data: new CommandBuilder({
@@ -14,12 +12,6 @@ const data = {
   }),
 
   async code(ctx) {
-    try {
-      const guild = ctx.guild;
-      if (!guild) return ctx.send("Solo se puede usar en servidores");
-
-      const warnId = ctx.args?.[0]?.toUpperCase();
-      if (!warnId) {
         const bot = ctx.bot.user;
         const paramerror = new EmbedBuilder()
           .setAuthor({ name: "Comando Removewarn", iconURL: bot.displayAvatarURL() })
@@ -31,28 +23,7 @@ const data = {
           .setColor(RED);
 
         return ctx.send({ embeds: [paramerror] });
-      }
-
-      if (!ctx.member.permissions.has(PermissionFlagsBits.ModerateMembers))
-        return ctx.send("No tienes el permiso `ModerateMembers`");
-
-      const warn = await Warn.findOneAndDelete({ guildId: guild.id, warnId });
-      if (!warn) return ctx.send(`No encontré la advertencia con ID \`${warnId}\``);
-
-      const user = await guild.client.users.fetch(warn.userId).catch(() => null);
-      const username = user?.globalName || user?.username || warn.userId;
-
-      const embed = new EmbedBuilder()
-        .setDescription(`Se eliminó la advertencia **\`${warnId}\`** de **${username}**`)
-        .setColor(GREEN)
-        .setTimestamp();
-
-      await ctx.send({ embeds: [embed] });
-      await sendLog(guild, embed);
-    } catch {
-      await ctx.send("No se pudo eliminar la advertencia");
-    }
-  },
+      },
 };
 
 module.exports = { data };
