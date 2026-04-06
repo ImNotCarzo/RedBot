8d85cc7d90cf28288ecea2976802fa6fceafd42a
carzo <118328562+ImNotCarzo@users.noreply.github.com>
2026-04-05 21:41:54 -0600

    Change dependency from 'erine' to 'gralonium'

--- commands/prefixed/utilping.js
+++ commands/prefixed/utilping.js
@@ -1,4 +1,4 @@
-const { CommandBuilder, ParamsBuilder } = require("erine");
+const { CommandBuilder, ParamsBuilder } = require("gralonium");
 const { EmbedBuilder } = require("discord.js");
 
 const data = {
